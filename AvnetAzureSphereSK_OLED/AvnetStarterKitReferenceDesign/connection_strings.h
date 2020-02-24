a2f0926d97ef058ab83d24eb2d1a433e7bcf8aa7
Walter Silvestre Coan <walter.coan@gmail.com>
2020-02-24 18:04:01 -0300

    AzureSphereAVNET_AirQuality_v2 new version for cmake compiler

---
+++ AvnetAzureSphereSK_OLED/AvnetStarterKitReferenceDesign/connection_strings.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Define your connection string here.  The connection string is required to connect to Azure.
+// #define MY_CONNECTION_STRING "HostName=iotc-xxxx....azure-devices.net;DeviceId=xxxx....;SharedAccessKey=xxxxx..."
+
+#define MY_CONNECTION_STRING "HostName=iotc..."
